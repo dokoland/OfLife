729a6902ac6e77053f2cc7caa53c95c3131dc6eb
tobias <tobias.pennekamp@gmail.com>
2021-12-07 00:01:27 +0100

    ✨ add benchmark project with BenchmarkDotNet

---
+++ Benchmark/BenchmarkGameOfLife.cs
@@ -0,0 +1,21 @@
+﻿namespace Benchmark;
+
+using BenchmarkDotNet.Attributes;
+using OfLife;
+
+public class BenchmarkGameOfLife
+{
+    private readonly MapBuilder _mapBuilder = 
+        A.Map
+         .WithWidth(120)
+         .WithHeight(40);
+
+    private Game _pentominoR => new(_mapBuilder.WithPentominoR());
+    private Game _spaceship => new(_mapBuilder.WithLeightWeightSpaceship());
+
+    [Benchmark]
+    public void LWSS() => _spaceship.Cycle(1000);
+
+    [Benchmark]
+    public void PentominoR() => _pentominoR.Cycle(1000);
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run(typeof(Program).Assembly);
+
